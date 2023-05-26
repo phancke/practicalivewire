b230a29786cf3a6a681e7b4a6b5e4eb724209e62
Phancke <pankedos@gmail.com>
2023-05-26 01:19:35 -0600

    calculadora practica, logica de componentes y binding

--- app/Http/Livewire/Calculator.php
+++ app/Http/Livewire/Calculator.php
@@ -6,8 +6,32 @@ use Livewire\Component;
 
 class Calculator extends Component
 {
+    public $val1,$val2,$operacion;
+    public $resultado=0;
+
+
     public function render()
     {
         return view('livewire.calculator');
+    }
+
+    public function operacion($operacion){
+           switch ($operacion){
+            case '+':
+                $variable=$this->val1 + $this->val2;
+                break;
+            case '-':
+                $this->resultado=$this->val1 - $this->val2;
+                break;
+            case '*':
+                $this->resultado=$this->val1 * $this->val2;
+                break;
+            case '/':
+                $this->resultado=$this->val1 / $this->val2;
+                break;
+        }
+
+        $this->resultado=$variable;
+
     }
 }
--- resources/views/livewire/calculator.blade.php
+++ resources/views/livewire/calculator.blade.php
@@ -1,24 +1,27 @@
+<div>
 <div class="flex justify-center ">
-    <x-input name="input1" class="border-2" ></x-input>
+    <label for="">{{$val1}} {{$resultado}}</label>
+    <x-input wire:model.defer="val1" name="input1" class="border-2" ></x-input>
     <x-dropdown align="false" >
         <x-slot name="trigger" >
-            <button class="mr-1 ml-1 align-content-center">Opciones</button>
+            <button class="mr-1 ml-1 align-content-center  rounded border-indigo-500 border-2 hover:bg-gray-500">Operaciones... </button>
         </x-slot>
-<x-slot name="content" class="mr-auto">
+<x-slot name="content" class="mr-3">
     <ul >
-        <li class="hover:bg-gray-100"><button wire:model="operacion">+</button></li>
-        <li class="hover:bg-gray-100"><button wire:model="operacion">-</button></li>
-        <li class="hover:bg-gray-100"><button wire:model="operacion">*</button></li>
-        <li class="hover:bg-gray-100"><button wire:model="operacion">/</button></li>
+        <li class="hover:bg-gray-100 text-center"><button wire:model="operacion">+</button></li>
+        <li class="hover:bg-gray-100 text-center"><button wire:model="operacion">-</button></li>
+        <li class="hover:bg-gray-100 text-center"><button wire:model="operacion">*</button></li>
+        <li class="hover:bg-gray-100 text-center"><button wire:model="operacion">/</button></li>
 
     </ul>
 </x-slot>
     </x-dropdown>
-    <x-input name="input2" class="inline-flex border-2"></x-input>
-    <x-button class="inline-block">=</x-button>
+     <x-input wire:model.defer="val2" name="input2" class="inline-flex border-2"></x-input>
+    <x-button wire:click="operacion('+')" class="inline-block">=</x-button>
 </div>
 
 <div class="text-center">
-    <x-label class="mt-2">Resultado</x-label>
-    <x-input name="input2" editable="false" class="inline-flex border-2 mt-2 read-only:true "></x-input>
+    <x-label  class="mt-2 text-2xl">Resultado </x-label>
+    <x-input  disabled="true" readonly="true" value="{{$resultado}}" class="inline-flex border-2 mt-2 read-only:true "></x-input>
+</div>
 </div>
